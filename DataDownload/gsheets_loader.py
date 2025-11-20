998440a99b7922a9af81919fdcf3cf9bc40ad4b2
Francesco <francesco.luciani10@gmail.com>
2025-11-20 09:27:34 +0100

    Prima versione

--- DataDownload/gsheets_loader.py
+++ DataDownload/gsheets_loader.py
@@ -1,3 +1,4 @@
+import os
 import gspread
 import pandas as pd
 from google.oauth2.service_account import Credentials
@@ -5,24 +6,42 @@ from google.oauth2.service_account import Credentials
 SHEET_URL = "https://docs.google.com/spreadsheets/d/1P6SV2hYDgfA0Nkv2FI9NA-siYOdB8YLsQnCv5QsFEhI/edit"
 
 TABS = [
-    "Europe GDP Forecast",
-    "Europe GDP Annual",
-    "Europe HICP Forecast",
-    "Europe HICP Annual",
+    "EuropeGDPForecast",
+    "EuropeGDPAnnual",
+    "EuropeHICPForecast",
+    "EuropeHICPAnnual",
     "Rates-Imported",
-    "EU HICP Data-Imported",
-    "EU GDP-Imported",
-    "US GDP Data-Imported",
-    "US PCE Data-Imported",
+    "EUHICPData-Imported",
+    "EUGDP-Imported",
+    "USGDPData-Imported",
+    "USPCEData-Imported",
+]
+
+POSSIBLE_PATHS = [
+    r"C:/Users/utente/Desktop/Personal/google.json",
+    r"C:/Users/franc/Documenti/google.json",
+    os.path.expanduser("~/google.json"),   # fallback generico
 ]
 
 
-def connect_to_google_sheet(credentials_path):
+def find_credentials_file():
+    for path in POSSIBLE_PATHS:
+        if os.path.exists(path):
+            return path
+    raise FileNotFoundError(
+        "Nessun file google.json trovato nei percorsi specificati. "
+        "Aggiungi il path corretto alla lista POSSIBLE_PATHS."
+    )
+
+
+def connect_to_google_sheet():
+    path = find_credentials_file()
+
     scopes = [
         "https://www.googleapis.com/auth/spreadsheets.readonly",
         "https://www.googleapis.com/auth/drive.readonly"
     ]
-    creds = Credentials.from_service_account_file(credentials_path, scopes=scopes)
+    creds = Credentials.from_service_account_file(path, scopes=scopes)
     return gspread.authorize(creds)
 
 
@@ -36,11 +55,13 @@ def load_single_tab(client, tab_name):
         return None
 
 
-def load_all_data(credentials_path="C:/Users/utente/Desktop/Personal/google.json"):
-    client = connect_to_google_sheet(credentials_path)
+def load_all_data():
+    client = connect_to_google_sheet()
     dfs = {}
+
     for tab in TABS:
         df = load_single_tab(client, tab)
         if df is not None:
             dfs[tab] = df
+
     return dfs
